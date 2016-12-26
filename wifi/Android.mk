0ad3e4493eb767c9167831ea7822236790e1cdda
audahadi <wan.audahadi@gmail.com>
2016-12-27 03:38:47 +0800

    add back wifi nv firmware

---
+++ wifi/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH:= $(call my-dir)
+
+ifeq ($(strip $(BOARD_HAS_QCOM_WLAN)),true)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := WCNSS_qcom_wlan_nv.bin
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/firmware/wlan/prima
+include $(BUILD_PREBUILT)
+
+endif
