5fa051454feafb76294a04889622c4e7a11bb3a0
audahadi <wan.audahadi@gmail.com>
2017-01-03 15:06:21 +0530

    rebrand to Lineage

--- lineage.mk
+++ lineage.mk
@@ -2,13 +2,13 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit from device specific
 $(call inherit-product, device/asus/Z00ED/full_Z00ED.mk)
 
-PRODUCT_NAME := cm_Z00ED
+PRODUCT_NAME := lineage_Z00ED
 PRODUCT_DEVICE := Z00ED
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone 2 Laser
